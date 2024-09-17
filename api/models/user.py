af6132c4e4ca935c01f011fcc333f949fdb0f538
Daniel Obiorah <Beetroit3266@gmail.com>
2024-09-17 07:12:59 +0100

    fix: fixed bug in models

--- api/models/user.py
+++ api/models/user.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+from datetime import datetime, timezone
+from uuid import uuid4
 
 from pydantic import UUID4
 from sqlalchemy import DateTime, Integer, String, Uuid
@@ -10,18 +11,18 @@ from models.base import Base
 class Users(Base):
     __tablename__ = "users"
 
-    id: Mapped[UUID4] = mapped_column(Uuid, primary_key=True)
+    id: Mapped[UUID4] = mapped_column(Uuid, primary_key=True, default=uuid4)
     full_name: Mapped[str] = mapped_column(String(100), nullable=False)
     phone_number: Mapped[str] = mapped_column(String(15), nullable=False)
-    username: Mapped[str] = mapped_column(String(30))
-    password: Mapped[str] = mapped_column(String)
-    public_key: Mapped[str] = mapped_column(String(100))
-    private_key: Mapped[str] = mapped_column(String(100))
-    wallet_alias: Mapped[str] = mapped_column(String(100))
-    transaction_pin: Mapped[int] = mapped_column(Integer)
-    sol_balance: Mapped[int] = mapped_column(Integer, nullable=True, default=0)
+    username: Mapped[str] = mapped_column(String(30), nullable=True)
+    password: Mapped[str] = mapped_column(String, nullable=True)
+    public_key: Mapped[str] = mapped_column(String(100), nullable=False)
+    private_key: Mapped[str] = mapped_column(String(100), nullable=False)
+    wallet_alias: Mapped[str] = mapped_column(String(100), nullable=True)
+    transaction_pin: Mapped[int] = mapped_column(Integer, nullable=True)
+    sol_balance: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
     created_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), nullable=False
+        DateTime(timezone=True), nullable=False, default=datetime.now(tz=timezone.utc)
     )
     last_balance_update: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), nullable=True
@@ -31,21 +32,21 @@ class Users(Base):
         self,
         full_name: str,
         phone_number: str,
-        username: str,
-        password: str,
         private_key: str,
         public_key: str,
-        wallet_alias: str,
-        transaction_pin: int,
+        username: str,
+        password: str = None,
+        wallet_alias: str = None,
+        transaction_pin: int = None,
     ):
         self.full_name = full_name
         self.phone_number = phone_number
         self.username = username
         self.password = password
         self.private_key = private_key
-        self.transaction_pin = transaction_pin
         self.public_key = public_key
         self.wallet_alias = wallet_alias
+        self.transaction_pin = transaction_pin
 
     def to_dict(self) -> dict[str, str | int]:
         return {
